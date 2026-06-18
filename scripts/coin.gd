81ddbb0fa70230ec9128af6424dea454ad3d56c0
FoxMcCloud77 <Denis2000Fomin@gmail.com>
2026-06-18 17:57:43 -0400

    Complete game
    - Add text
    - Add sounds
    - Improve all game mechanics

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var game_manager = %GameManager
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Coin +1!")
-	queue_free()
+	game_manager.add_score()
+	animation_player.play("pickup")
